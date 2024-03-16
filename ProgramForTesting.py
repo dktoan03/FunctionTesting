4b849da21d40033a059c9708b723ae96b0648ed7
dktoan03 <139577081+dktoan03@users.noreply.github.com>
2024-03-17 01:34:19 +0700

    Update ProgramForTesting.py

--- ProgramForTesting.py
+++ ProgramForTesting.py
@@ -6,6 +6,10 @@ with open("DecisionTableTesting.txt", "r") as file_object:
     file_content = file_object.read()
 pairs_DecisionTableTesting = eval(file_content)
 
+with open("ControlFlowTesting.txt", "r") as file_object:
+    file_content = file_object.read()
+pairs_ControlFlowTesting = eval(file_content)
+
 for IMDB, HOURSTART in pairs_BoundaryValueAnalysis:
     if (IMDB < 0) or (IMDB > 10) or (HOURSTART < 0) or (HOURSTART > 24):
         print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", -1)
@@ -17,6 +21,16 @@ for IMDB, HOURSTART in pairs_BoundaryValueAnalysis:
         print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 2)
 
 for IMDB, HOURSTART in pairs_DecisionTableTesting:
+    if (IMDB < 0) or (IMDB > 10) or (HOURSTART < 0) or (HOURSTART > 24):
+        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", -1)
+    elif (IMDB < 6) or (IMDB < 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
+        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 0)
+    elif ((IMDB < 8.5) and (HOURSTART >= 12 and HOURSTART <= 16)) or (IMDB > 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
+        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 1)
+    else:
+        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 2)
+        
+for IMDB, HOURSTART in pairs_ControlFlowTesting:
     if (IMDB < 0) or (IMDB > 10) or (HOURSTART < 0) or (HOURSTART > 24):
         print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", -1)
     elif (IMDB < 6) or (IMDB < 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
