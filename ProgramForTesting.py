896abfe3b5b55e5df8ce81d4a1d0ff587d5edd88
dktoan03 <139577081+dktoan03@users.noreply.github.com>
2024-03-17 17:20:49 +0700

    Update ProgramForTesting.py

--- ProgramForTesting.py
+++ ProgramForTesting.py
@@ -10,19 +10,17 @@ with open("ControlFlowTesting.txt", "r") as file_object:
     file_content = file_object.read()
 pairs_ControlFlowTesting = eval(file_content)
 
-def Test(PAIR)
+def Test(PAIR):
     for IMDB, HOURSTART in PAIR:
-    if (IMDB < 0) or (IMDB > 10) or (HOURSTART < 0) or (HOURSTART > 24):
-        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", -1)
-    elif (IMDB < 6) or (IMDB < 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
-        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 0)
-    elif ((IMDB < 8.5) and (HOURSTART >= 12 and HOURSTART <= 16)) or (IMDB > 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
-        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 1)
-    else:
-        print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 2)
+        if (IMDB < 0) or (IMDB > 10) or (HOURSTART < 0) or (HOURSTART > 24):
+            print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", -1)
+        elif (IMDB < 6) or (IMDB < 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
+            print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 0)
+        elif ((IMDB < 8.5) and (HOURSTART >= 12 and HOURSTART <= 16)) or (IMDB > 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
+            print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 1)
+        else:
+            print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 2)
         
 Test(pairs_BoundaryValueAnalysis)
 Test(pairs_DecisionTableTesting)
 Test(pairs_ControlFlowTesting)
-
-
