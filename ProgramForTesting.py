675f2ac59b2f75ce30d31eae62508fa75c849150
dktoan03 <139577081+dktoan03@users.noreply.github.com>
2024-03-29 00:46:00 +0700

    Update ProgramForTesting.py

--- ProgramForTesting.py
+++ ProgramForTesting.py
@@ -1,31 +1,26 @@
-with open("BoundaryValueAnalysis.txt", "r") as file_object:
-    file_content = file_object.read()
-pairs_BoundaryValueAnalysis = eval(file_content)
-
-with open("DecisionTableTesting.txt", "r") as file_object:
-    file_content = file_object.read()
-pairs_DecisionTableTesting = eval(file_content)
-
-with open("ControlFlowTesting.txt", "r") as file_object:
-    file_content = file_object.read()
-pairs_ControlFlowTesting = eval(file_content)
-
-with open("DataFlowTesting.txt", "r") as file_object:
-    file_content = file_object.read()
-pairs_DataFlowTesting = eval(file_content)
-
 def Test(PAIR):
     for IMDB, HOURSTART, ISFAVORITE in PAIR:
         if (IMDB < 0) or (IMDB > 10) or (HOURSTART < 0) or (HOURSTART > 24):
             print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", -1)
-        elif (!ISFAVORITE) or (IMDB < 6) or (IMDB < 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
+        elif (not ISFAVORITE) or (IMDB < 6) or (IMDB < 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
             print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 0)
         elif ((IMDB < 8.5) and (HOURSTART >= 12 and HOURSTART <= 16)) or (IMDB > 8.5 and (HOURSTART < 12 or (HOURSTART > 16 and HOURSTART < 20))):
             print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 1)
         else:
             print(f"IMDB = {IMDB}, HOURSTART = {HOURSTART}", "output = ", 2)
-        
-Test(pairs_BoundaryValueAnalysis)
-Test(pairs_DecisionTableTesting)
-Test(pairs_ControlFlowTesting)
-Test(pairs_DataFlowTesting)
+
+def main(filename):
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                # Chuyển đổi dòng thành tuple và chạy hàm Test
+                # Đây là cách đơn giản, nhưng cẩn thận với dữ liệu đầu vào không tin cậy
+                pair = eval(line.strip())
+                Test(pair)
+    except FileNotFoundError:
+        print("File not found!")
+
+main('BoundaryValueAnalysis.txt')
+main('ControlFlowTesting.txt')
+main('DataFlowTesting.txt)
+main('DecisionTableTesting.txt')
